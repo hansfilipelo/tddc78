1d6f28d6634c70db294e9da95e698c4b37b1beb7
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-05-03 16:49:52 +0200

    Almost works..

--- lab2/src/laplsolv.f90
+++ lab2/src/laplsolv.f90
@@ -1,70 +1,74 @@
 program laplsolv
 
 
-!-----------------------------------------------------------------------
-! Serial program for solving the heat conduction problem
-! on a square using the Jacobi method.
-! Written by Fredrik Berntsson (frber@math.liu.se) March 2003
-! Modified by Berkant Savas (besav@math.liu.se) April 2006
-!-----------------------------------------------------------------------
-  integer, parameter                  :: n=1000, maxiter=1000
-  double precision,parameter          :: tol=1.0E-3
-  double precision,dimension(0:n+1,0:n+1) :: T
-  double precision,dimension(n)       :: tmp1,tmp2
-  double precision                    :: error,x
-  real                                :: t1,t0
-  integer                             :: i,j,k
-  character(len=20)                   :: str
+    !-----------------------------------------------------------------------
+    ! Serial program for solving the heat conduction problem
+    ! on a square using the Jacobi method.
+    ! Written by Fredrik Berntsson (frber@math.liu.se) March 2003
+    ! Modified by Berkant Savas (besav@math.liu.se) April 2006
+    !-----------------------------------------------------------------------
+    integer, parameter                  :: n=1000, maxiter=1000
+    double precision,parameter          :: tol=1.0E-3
+    double precision,dimension(0:n+1,0:n+1) :: T
+    double precision,dimension(n)       :: tmp1,tmp2
+    double precision                    :: error,x
+    real                                :: t1,t0
+    integer                             :: i,j,k
+    character(len=20)                   :: str
+    logical                             :: running=.TRUE.
 
-  ! Set boundary conditions and initial values for the unknowns
-  T=0.0D0
-  T(0:n+1 , 0)     = 1.0D0
-  T(0:n+1 , n+1)   = 1.0D0
-  T(n+1   , 0:n+1) = 2.0D0
+    ! Set boundary conditions and initial values for the unknowns
+    T=0.0D0
+    T(0:n+1 , 0)     = 1.0D0
+    T(0:n+1 , n+1)   = 1.0D0
+    T(n+1   , 0:n+1) = 2.0D0
 
 
-  ! Nr threads
-  !write(*,*) 'Nr threads: ',OMP_GET_THREAD_NUMB()
-  !write(*,*)
+    ! Nr threads
+    !write(*,*) 'Nr threads: ',OMP_GET_THREAD_NUM()
+    !write(*,*)
 
-  ! Solve the linear system of equations using the Jacobi method
-  call cpu_time(t0)
+    ! Solve the linear system of equations using the Jacobi method
+    call cpu_time(t0)
 
-  do k=1,maxiter
+    !$omp parallel do private(j,tmp1,tmp2,error) lastprivate(k) shared(T, done)
 
-     tmp1=T(1:n,0)
-     error=0.0D0
+    do k=1,maxiter
 
-     !$omp parallel do private(j,tmp1,tmp2) shared(k,error,T)
+        if ( running ) then
 
-     do j=1,n
-        tmp2=T(1:n,j)
-        T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1)/4.0D0
-        error=max(error,maxval(abs(tmp2-T(1:n,j))))
-        tmp1=tmp2
-     end do
+            tmp1=T(1:n,0)
+            error=0.0D0
 
-     !$omp end parallel do
+            do j=1,n
+                if (error<tol .AND. j>1) then
+                    running=.FALSE.
+                    exit
+                end if
 
-     if (error<tol) then
-        exit
-     end if
+                tmp2=T(1:n,j)
+                T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1)/4.0D0
+                error=max(error,maxval(abs(tmp2-T(1:n,j))))
+                tmp1=tmp2
+            end do
+        end if
 
-  end do
+    end do
+    !$omp end parallel do
 
-  call cpu_time(t1)
+    call cpu_time(t1)
 
-  write(unit=*,fmt=*) 'Time:',t1-t0,'Number of Iterations:',k
-  write(unit=*,fmt=*) 'Temperature of element T(1,1)  =',T(1,1)
+    write(unit=*,fmt=*) 'Time:',t1-t0,'Number of Iterations:',k
+    write(unit=*,fmt=*) 'Temperature of element T(1,1)  =',T(1,1)
 
-  ! Uncomment the next part if you want to write the whole solution
-  ! to a file. Useful for plotting.
+    ! Uncomment the next part if you want to write the whole solution
+    ! to a file. Useful for plotting.
 
-  !open(unit=7,action='write',file='result.dat',status='unknown')
-  !write(unit=str,fmt='(a,i6,a)') '(',N,'F10.6)'
-  !do i=0,n+1
-  !   write (unit=7,fmt=str) T(i,0:n+1)
-  !end do
-  !close(unit=7)
+    !open(unit=7,action='write',file='result.dat',status='unknown')
+    !write(unit=str,fmt='(a,i6,a)') '(',N,'F10.6)'
+    !do i=0,n+1
+    !   write (unit=7,fmt=str) T(i,0:n+1)
+    !end do
+    !close(unit=7)
 
 end program laplsolv
