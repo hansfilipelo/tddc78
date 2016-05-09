3d0d3e3e88cad454137242fe1b1a1ff4f2725b4b
Frida Sundberg <sundberg.fr@gmail.com>
2016-05-09 12:21:21 +0200

    This code doesn´t work, can´t figure out how to parallelize a recursive algorithm

--- lab2/src/laplsolv.f90
+++ lab2/src/laplsolv.f90
@@ -1,5 +1,5 @@
 program laplsolv
-
+    use omp_lib
 
     !-----------------------------------------------------------------------
     ! Serial program for solving the heat conduction problem
@@ -7,15 +7,15 @@ program laplsolv
     ! Written by Fredrik Berntsson (frber@math.liu.se) March 2003
     ! Modified by Berkant Savas (besav@math.liu.se) April 2006
     !-----------------------------------------------------------------------
-    integer, parameter                  :: n=1000, maxiter=1000
-    double precision,parameter          :: tol=1.0E-3
+    integer, parameter                      :: n=1000, maxiter=1000, nr_threads=4
+    double precision,parameter              :: tol=1.0E-3
     double precision,dimension(0:n+1,0:n+1) :: T
-    double precision,dimension(n)       :: tmp1,tmp2
-    double precision                    :: error,x
-    real                                :: t1,t0
-    integer                             :: i,j,k
-    character(len=20)                   :: str
-    logical                             :: running=.TRUE.
+    double precision,dimension(n)           :: tmp1,tmp2
+    double precision                        :: error,x
+    real                                    :: t1,t0
+    integer                                 :: i,j,k,chunk_size
+    character(len=20)                       :: str
+    double precision,dimension(0:n-1,0:nr_threads-1) :: padding
 
     ! Set boundary conditions and initial values for the unknowns
     T=0.0D0
@@ -23,38 +23,40 @@ program laplsolv
     T(0:n+1 , n+1)   = 1.0D0
     T(n+1   , 0:n+1) = 2.0D0
 
-
-    ! Nr threads
-    !write(*,*) 'Nr threads: ',OMP_GET_THREAD_NUM()
-    !write(*,*)
+    chunk_size = n/nr_threads
 
     ! Solve the linear system of equations using the Jacobi method
     call cpu_time(t0)
 
-    !$omp parallel do private(j,tmp1,tmp2,error) lastprivate(k) shared(T, done)
-
     do k=1,maxiter
 
-        if ( running ) then
+        tmp1=T(1:n,0)
+        error=0.0D0
 
-            tmp1=T(1:n,0)
-            error=0.0D0
+        do i=0,nr_threads-1
+            padding(0:n-1,i) = T(1:n,(i+1)*chunk_size+1)
+        end do
 
-            do j=1,n
-                if (error<tol .AND. j>1) then
-                    running=.FALSE.
-                    exit
-                end if
+        !$omp parallel do schedule(STATIC,chunk_size) private(j,tmp1,tmp2) firstprivate(padding) shared(T) reduction(max: error)
+        do j=1,n
+            tmp2=T(1:n,j) !! FEEEEEEL
+            if((.NOT. j==chunk_size*(OMP_GET_THREAD_NUM()+1)) .OR. j==n) then
+                T(1:n,j) =(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1)/4.0D0
+            else
+                T(1:n,j) =(T(0:n-1,j)+T(2:n+1,j)+padding(0:n-1,OMP_GET_THREAD_NUM())+tmp1)/4.0D0
+            end if
+            error=max(error,maxval(abs(tmp2-T(1:n,j))))
+            tmp1=tmp2
+        end do
+        !$omp end parallel do
 
-                tmp2=T(1:n,j)
-                T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1)/4.0D0
-                error=max(error,maxval(abs(tmp2-T(1:n,j))))
-                tmp1=tmp2
-            end do
+        if (error<tol) then
+            write(unit=*,fmt=*) 'k = ',k,' , threadnr = ',OMP_GET_THREAD_NUM()
+            write(unit=*,fmt=*) 'error = ',error
+            exit
         end if
 
     end do
-    !$omp end parallel do
 
     call cpu_time(t1)
 
